0c236101e89b6f97d91203fe85cabeba61802f21
Adolfo <fitox.metallica@gmail.com>
2020-04-19 14:59:10 -0500

    INCLUI FUNCIONALIDAD DE ENVIAR CORREOS EN ABOUT

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,11 +1,53 @@
 const express = require('express');
 const router = express.Router();
-//obejto para crear rutas
-
 const Post = require('../models/Posts');
+//obejto para crear rutas
+var nodemailer = require('nodemailer'); 
+
+
+require('dotenv').config({ path: 'variables.env' });
 
 
 
+router.post('/send', (req, res) => {
+    //console.log(req.body);
+    const output = `
+    <p>You have a new contact request</p>
+    <h3>contact details</h3>
+    <ul>
+    <li>Name: ${req.body.name}</li>
+    <li>email: ${req.body.email}</li>
+    <li>asunto: ${req.body.asunto}</li>
+ 
+    </ul>
+        <p>mensaje: ${req.body.message}</p>
+    `;
+
+    let transporter = nodemailer.createTransport({
+        service: 'gmail',
+        
+        auth: {
+            user: process.env.USER_PSW,
+            pass: process.env.PSW_PSW
+        }
+    });
+    var mailOptions = {
+        from: 'Michael Adolfo Huerta Ramirez',
+        to: process.env.USER_PSW,
+        subject: 'Asunto',
+        text: 'Contenido del email',
+        html: output
+    };
+
+    transporter.sendMail(mailOptions, function (error, info) {
+        if (error) {
+            console.log(error);
+        } else {
+            console.log('Email enviado: ' + info.response);
+            res.render('about',{msg:'Se ha enviado correctamente el correo'});
+        }
+    });
+});
 //Aqui solo saldran los mejores post
 router.get('/', async (req, res, next) => {
     const entradas = await Post.find()
@@ -17,6 +59,7 @@ router.get('/', async (req, res, next) => {
 router.get('/about', (req, res, next) => {
     res.render('about');
 });
+
 
 
 
